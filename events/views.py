44e4913acdabc1b9b5b648e5d4b821bbee409872
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-10-30 18:36:11 +0200

    add feature for join, leave event partisipation

--- events/views.py
+++ events/views.py
@@ -1,5 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
+from rest_framework import viewsets, status, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from events.filters import EventFilter
 from events.models import Event
@@ -22,5 +24,41 @@ class EventViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = self.queryset
         if self.action in ["list", "retrieve"]:
-            return self.queryset.select_related()
+            return self.queryset.prefetch_related("participants")
         return queryset
+
+    @action(
+        detail=True,
+        methods=["get"],
+        url_path="join",
+        url_name="join_event",
+        permission_classes=[permissions.IsAuthenticated])
+    def join_event(self, request, pk=None):
+        event = self.get_object()
+        user = request.user
+
+        if event.participants.filter(id=user.id).exists():
+            return Response({"detail": "You are already a participant of this event."},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        event.participants.add(user)
+        event.save()
+
+        return Response({"detail": "You have joined the event successfully."}, status=status.HTTP_200_OK)
+
+    @action(
+        detail=True,
+        methods=["get"],
+        url_path="leave",
+        url_name="leave_event")
+    def leave_event(self, request, pk=None):
+        event = self.get_object()
+        user = request.user
+
+        if not event.participants.filter(id=user.id).exists():
+            return Response({"detail": "You are not a participant of this event."}, status=status.HTTP_400_BAD_REQUEST)
+
+        event.participants.remove(user)
+        event.save()
+
+        return Response({"detail": "You have left the event successfully."}, status=status.HTTP_200_OK)
