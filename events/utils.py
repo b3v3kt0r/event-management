ea74920eab64801383b044ce3c32b8899faced04
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-10-30 23:28:53 +0200

    fix utils

--- events/utils.py
+++ events/utils.py
@@ -1,9 +1,23 @@
+import os
+
 from django.core.mail import send_mail
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def send_event_registration_email(user, event):
     subject = f"Registration for the {event.title} event is successful."
-    message = f"Hello, {user.username}!\n\nWe are waiting for you on {event.date}. \n\nThanks for your interest!"
+
+    event_date_formatted = event.date.strftime("%d %B %Y о %H:%M")
+
+    message = (
+        f"Hello, {user.username}!\n\n"
+        f"We are waiting for you on {event_date_formatted}.\n\n"
+        "Thanks for your interest!"
+    )
     recipient_list = [user.email]
 
-    send_mail(subject, message, "stanislav.v.sudakov@gmail.com", recipient_list)
+    email_from = os.environ.get("EMAIL_HOST_USER")
+
+    send_mail(subject, message, email_from, recipient_list)
