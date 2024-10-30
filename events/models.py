85a326cc25bd2346611a042c5c94069be85a1d12
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-10-30 15:40:45 +0200

    add base model for events

--- events/models.py
+++ events/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=100)
+    organiser = models.CharField(max_length=100)
+    participants = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name="events")
+
+    def __str__(self):
+        return self.title
