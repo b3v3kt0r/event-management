2e2ca0fd18bc4ccea33eb5bfd1f7ec3a417dea6d
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-10-30 22:46:22 +0200

    add feature for sending email

--- event_management/settings.py
+++ event_management/settings.py
@@ -161,3 +161,11 @@ SPECTACULAR_SETTINGS = {
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
