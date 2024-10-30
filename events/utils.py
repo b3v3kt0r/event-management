ea30cfef1e4cbc3e27173956329f60a39a555753
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-10-30 22:47:41 +0200

    fix utils

--- events/utils.py
+++ events/utils.py
@@ -3,7 +3,7 @@ from django.core.mail import send_mail
 
 def send_event_registration_email(user, event):
     subject = f"Registration for the {event.title} event is successful."
-    message = f"Hello, {user.username}!\n\nWe are waiting for you on {event.date}. \n\nThanks for your interesting!"
+    message = f"Hello, {user.username}!\n\nWe are waiting for you on {event.date}. \n\nThanks for your interest!"
     recipient_list = [user.email]
 
     send_mail(subject, message, "stanislav.v.sudakov@gmail.com", recipient_list)
