f6e698f846ba4b301b564943216198cb40e76518
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-10-30 16:54:17 +0200

    update views for user

--- user/views.py
+++ user/views.py
@@ -1,11 +1,11 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAdminUser
 
 from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = []
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
